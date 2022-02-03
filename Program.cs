f120d282564975d525619e0f5a05294d932e3a60
netonblpb <skullbog@gmail.com>
2022-02-03 13:31:20 +0300

    Task3. Complete

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//Task3: По заданному номеру дня недели вывести его название
+
+string[] weekday = new string[] {"пн", "вт", "ср", "чт", "пт", "сб", "вс"};
+
+Console.Write("Введите номер дня недели (1 - 7): ");
+string x = Console.ReadLine();
+
+Console.WriteLine(weekday[Convert.ToInt16(x)-1]);
